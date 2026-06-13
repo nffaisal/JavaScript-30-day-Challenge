7680a93c6e6ae137eb26cf103d6d15cd17898504
Noor <noorffatima0045@gmail.com>
2026-06-14 00:48:00 +0500

    fixed the clock hand and working on javascript  to make the hands move according to the time

--- MovingClock/clockScript.js
+++ MovingClock/clockScript.js
@@ -1,17 +1,31 @@
-   const minutes =now.getMinutes; 
+ function calculateDegree(number){
+    return ((number/60)* 360); 
+}
+ /*for the seconds hand*/
    let secondHand =document.getElementById("second-hand");
+   const now = new Date();
 function setDate(){
-    const now = new Date();
     const seconds = now.getSeconds();
-    console.log(seconds);
-    const secondsDegree = (seconds/60) *360;
+   // console.log(seconds);
+    const secondsDegree = 
     secondHand.style.transform = `rotate(${secondsDegree}deg)`;
-    console.log(secondsDegree);
+   // console.log(secondsDegree);
 }
 
-const minuteHand =document.getElementById("minute-hand");
-function sethour(){
+ /* for the minute hand*/
+  const minutes =now.getMinutes; 
+  const minuteHand =document.getElementById("minute-hand");
+  function setminute(){
     const minutes = now.getMinutes();
+    const minutesDegree = calculateDegree(minutes);
+    minuteHand.style.transform =`rotate(${minutesDegree})`;
 
 }
-setInterval(setDate, 1000);
+setInterval(setminute,100000);
+
+const hourHand =document.getElementById("hour-hand"); 
+function sethour(){
+     const hours= now.getHours();
+     const hours
+}
+sethour();
