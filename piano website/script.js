927bc40f035da79f013d635249bc8de60b88213a
Noor <noorffatima0045@gmail.com>
2026-06-13 19:47:58 +0500

    added  javascript keydown and up event listeners, and css styling

--- piano website/script.js
+++ piano website/script.js
@@ -1,3 +1,23 @@
-window.addEventListener( 'keydown', function(e){
-    console.log(e.key);
-} )
+
+ const audioMap ={};
+ document.querySelectorAll(`audio[data-key]`).forEach(
+    audio => {
+        const key = audio.getAttribute("data-key");
+        audioMap[key] = audio;
+    }
+ )
+ window.addEventListener( 'keydown', function(e){
+    const audio = audioMap[e.code];
+    const pianoKey = this.document.querySelector(`[data-key="${e.code}"]`);
+    if(!audio){ return;} //if no audio element matches the function
+    audio.currentTime =0;
+    audio.play();
+    pianoKey.classList.add("playing");
+}
+ )
+ window.addEventListener( 'keyup', function(e) {
+   const pianoKey = this.document.querySelector(`[data-key="${e.code}"]`);  
+  pianoKey.classList.remove("playing");
+
+ })
+ 
